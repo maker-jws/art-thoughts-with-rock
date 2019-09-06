ef319ae0f638cc4796d5a30a14c62de500f70fd0
Joshua Smith <ws.joshua@gmail.com>
2019-09-06 10:59:25 -0600

    retrieving query information

--- src/Components/MainContainer/index.js
+++ src/Components/MainContainer/index.js
@@ -10,12 +10,16 @@ class MainContainer extends Component {
         this.state = {
             searchHistory: [],
             parsedSearch: {},
+            requestQuery: {}, //the keys to be sent to db for 'Data'
+            requestResultsUrl: {}, //the keys to be sent to db for 'Source'
             lastSearch: "",
             allResults: [],
             chosenResults: [],
             resultsLoaded: false,
             resultsLoading: false,
-            resultsToRender: []
+            resultsToRender: [],
+            search_num: 0,
+            user_id: 0,
         }
     }
     handleSearchSubmit = async (query) => {
@@ -32,6 +36,32 @@ class MainContainer extends Component {
                 //r.set("searchHistory")
             })
         }
+        catch (err) {
+            console.log(err)
+        }
+    }
+    // requestQuery: {}, //the keys to be sent to db for 'Data'
+    // requestResultsUrl: {},
+    reqQuery = async (source) => {
+        try {
+            // console.log('source in reqQuery', source);
+            const queryPackage = {
+                query_string: source.queries.request[0].searchTerms,              //CharField(null=False)
+                exclusions: "pinterest, deviantart, artnews",                //CharField(null=True)
+                cached_ID: source.queries.request[0].cx,                 //CharField(null=True),
+                initial_value: 0,             //IntegerField(default=0, null=True),
+                search_num: this.state.search_num,
+                was_selected: false,
+                search_num: parseInt(source.queries.request[0].totalResults),
+                user_id: this.state.user_id,
+            }
+            console.log(queryPackage, "after establishing content - line 58 -MainContainer")
+        } catch (err) {
+            console.log(err)
+        }
+    }
+    reqResultsUrl = async (source) => {
+        try { console.log('source in reqResultsUrl', source) }
         catch (err) {
             console.log(err)
         }
@@ -78,7 +108,7 @@ class MainContainer extends Component {
                 q = this.state.parsedSearch.targetStrings[0];
             }
             const api_key = "AIzaSyCyVfsN9ihaglSFcP9SM-NQwdzlnFFOsys"
-            console.log(this.state.searchHistory)
+            // console.log(this.state.searchHistory)
             const responseQuery = await fetch("https://www.googleapis.com/customsearch/v1?key=" + api_key + "&cx=013070184471859259983%3Aakjlb1b5hvu&q=" + q, {
                 method: "GET",
                 credentials: "include",
@@ -91,13 +121,16 @@ class MainContainer extends Component {
             } else {
                 const temp = [];
                 const searchQueryResponse = await responseQuery.json();
-                console.log(searchQueryResponse, "query response");
+                // console.log(searchQueryResponse, "query response");
+                this.reqQuery(searchQueryResponse);
                 searchQueryResponse.items.map((item, idx) => {
                     // console.log(item);
                     return temp.push(item);
                 });
                 this.setState({ allResults: temp }, () => {
-                    this.filteredItems(temp)
+                    this.filteredItems(temp);
+                    this.reqResultsUrl(temp);
+                    //call function that sends all urls to 
                 })
             }
         } catch (err) {
